7b4370b6dcbbec50f49d8f3373fee8c0654fff1b
Vaffi528 <alexeytr995577@gmail.com>
2026-08-24 23:25:16 +0300

    fix: re-fulfilled task-04 with the array methods from task-02

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,4 +1,5 @@
 import { Todo } from './types';
+import { mapArray, filterArray } from './array-helpers';
 
 function hasTodoWithId(state: Todo[], id: number): boolean {
   const isExist = state.some((value: Todo) => value.id === id);
@@ -15,7 +16,7 @@ export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo,
     throw new Error(`updateTodo: There are no element with such id: ${id}`)
   }
 
-  return state.map((value: Todo) => value.id === id ? <Todo>{...value, ...update} : value);
+  return mapArray(state, (value: Todo) => value.id === id ? <Todo>{...value, ...update} : value);
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
@@ -23,7 +24,7 @@ export function removeTodo(state: Todo[], id: number): Todo[] {
     throw new Error(`removeTodo: There are no element with such id: ${id}`)
   }
 
-  return state.filter((value: Todo) => value.id !== id);
+  return filterArray(state, (value: Todo) => value.id !== id);
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
@@ -31,5 +32,5 @@ export function getTodo(state: Todo[], id: number): Todo | undefined {
     throw new Error(`removeTodo: There are no element with such id: ${id}`)
   }
 
-  return state.find((value: Todo) => value.id === id);
+  return filterArray(state, (value: Todo) => value.id === id)[0];
 }
