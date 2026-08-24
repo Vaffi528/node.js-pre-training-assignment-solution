e72c5153dc95009a2a9634ea367c5afdae1f780e
Vaffi528 <alexeytr995577@gmail.com>
2026-08-24 23:19:00 +0300

    feat: fulfilled task-04 - implemented addTodo, updateTodo, removeTodo and getTodo functions

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,35 @@
 import { Todo } from './types';
 
+function hasTodoWithId(state: Todo[], id: number): boolean {
+  const isExist = state.some((value: Todo) => value.id === id);
+  return isExist
+}
 export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
+  let resultArray: Todo[] = [];
+  resultArray.push(todo);
+  return resultArray;
 }
 
 export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+  if (!hasTodoWithId(state, id)) {
+    throw new Error(`updateTodo: There are no element with such id: ${id}`)
+  }
+
+  return state.map((value: Todo) => value.id === id ? <Todo>{...value, ...update} : value);
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
-  throw new Error('removeTodo: not implemented');
+  if (!hasTodoWithId(state, id)) {
+    throw new Error(`removeTodo: There are no element with such id: ${id}`)
+  }
+
+  return state.filter((value: Todo) => value.id !== id);
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+  if (!hasTodoWithId(state, id)) {
+    throw new Error(`removeTodo: There are no element with such id: ${id}`)
+  }
+
+  return state.find((value: Todo) => value.id === id);
 }
