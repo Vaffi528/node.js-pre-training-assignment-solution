87091e506ef4b1ff2f7ff21bb33180d690add6ec
Vaffi528 <alexeytr995577@gmail.com>
2026-09-16 10:04:23 +0300

    feat: fulfill task-10 - implement TodoManager and CLI

--- JS-TS/solutions/index.ts
+++ JS-TS/solutions/index.ts
@@ -1,3 +1,74 @@
-#!/usr/bin/env ts-node
-// CLI entry for Task 10 – placeholder only
-console.log('CLI not implemented yet');
+import { ToDoManager } from "./todo-manager";
+import { TodoStatus } from "./types";
+
+export class CLI {
+    private manager = new ToDoManager;
+
+    async exec(args: string[] = process.argv.slice(2)): Promise<void> {
+        const command = args[0];
+
+        if (!command) return;
+
+        await this.manager.init();
+
+        switch (command) {
+            case 'add':
+                await this.addFunc(args[1], args[2]);
+                break;
+            case 'complete':
+                await this.completeFunc(args[1]);
+                break;
+            case 'list':
+                await this.listFunc();
+                break;
+            default:
+                throw new Error("exec: Invalid command");
+        }
+    }
+
+    async addFunc(title: string, description: string): Promise<void> {
+        if (!title) throw new Error("addFunc: Invalid argument");
+                
+        if (description) {
+            await this.manager.add(title, description);
+        } else {
+            await this.manager.add(title);
+        }
+
+        console.log(`Todo is successfully added: ${title}, ${description ?? "(no description)"}`);
+    }
+
+    async completeFunc(id: string): Promise<void> {
+        if (!id) throw new Error("completeFunc: Invalid argument");
+        
+        const idInt = parseInt(id);
+        if (isNaN(idInt)) 
+            throw new Error("completeFunc: Invalid id: must be number");
+
+        await this.manager.complete(idInt);
+
+        console.log(`Status of todo with id ${id} is successfully toggled`);
+    }
+
+    async listFunc(): Promise<void> {
+        const todos = await this.manager.list();
+        
+        if (todos.length == 0) 
+            console.log("List of todos is empty");
+
+        todos.forEach(element => {
+            let statusAsStirng: string;
+            if (element.status == TodoStatus.COMPLETED)
+                statusAsStirng = 'completed';
+            else if (element.status == TodoStatus.PENDING)
+                statusAsStirng = 'pending';
+            else
+                statusAsStirng = 'in progress';
+            console.log(`Todo: ${element.id}, ${element.title}, ${element.createdAt.toString()}, ${statusAsStirng}, ${element.description ?? "-"}`);
+        });
+    }
+}
+
+
+const cli = new CLI();
+cli.exec().catch((err) => console.log(`${err.message}`));
--- JS-TS/solutions/todo-manager.ts
+++ JS-TS/solutions/todo-manager.ts
@@ -3,21 +3,31 @@ import { TodoApi } from './todo-api';
 import { Todo } from './types';
 
 export class ToDoManager {
-  private service = new TodoService(new TodoApi());
+  private api = new TodoApi()
+  private service = new TodoService(this.api);
+  private isInit = false;
 
   async init(): Promise<void> {
-    throw new Error('init: not implemented');
+    if (this.isInit) {
+      return;
+    }
+
+    await this.service.create("Todo1", "descr");
+    await this.service.create("Todo2", "67");
+    await this.service.create("Todo3");
+    await this.service.create("Todo4");
+    await this.service.create("Todo5", "42");
   }
 
   async add(title: string, description = ''): Promise<void> {
-    throw new Error('add: not implemented');
+    await this.service.create(title, description);
   }
 
   async complete(id: number): Promise<void> {
-    throw new Error('complete: not implemented');
+    await this.service.toggleStatus(id);
   }
 
   async list(): Promise<Todo[]> {
-    throw new Error('list: not implemented');
+    return await this.api.getAll();
   }
 }
